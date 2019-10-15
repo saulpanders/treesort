89afc6a8f1cdfa5766d7bbda2415b42f69f1d07a
Saul Panders <phs3mr@virginia.edu>
2019-10-15 16:02:39 -0400

    initial commit - porting old code

---
+++ treesort.go
@@ -0,0 +1,76 @@
+/*
+	Paul Sanders
+	@saulpanders
+	treesort.go -- testing recursive data structs & tree review
+	8/29/18
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// basic tree struct
+type tree struct {
+	val         int
+	left, right *tree
+}
+
+//to use, type ints into command line you want sorted (seperated by spaces)
+//will return low to high
+func main() {
+
+	var values []int
+	for i := 1; i < len(os.Args); i++ {
+		//strconv library handles int-string stuff
+		arg, err := strconv.Atoi(os.Args[i])
+		if err != nil {
+			//handle error
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		//append is a built in modifier, sweet!
+		values = append(values, arg)
+	}
+
+	//treesort our array
+	Sort(values)
+	fmt.Println(values)
+	//sorts values in place
+}
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		//equivalent to return &tree{value: value}
+		t = new(tree)
+		t.val = value
+		return t
+	}
+
+	if value < t.val {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.val)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
